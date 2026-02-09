9ef37fac2021176e2f96709aa90af13f743bc788
panuwat <khumpeerawat@gmail.com>
2026-02-10 00:17:40 +0700

    Query parameter Exc1

--- querying-all-records-with-query-parameters-exercise-1/app.mjs
+++ querying-all-records-with-query-parameters-exercise-1/app.mjs
@@ -7,27 +7,24 @@ const port = 4000;
 app.use(express.json());
 
 app.get("/movies", async (req, res) => {
-	try {
-		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
-		const genresParam = null;
-		const result = await pool.query(
-			`
-    SELECT * FROM movies
-    `,
-			[]
-		);
-		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
+  try {
+    // แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
+    const genresParam = req.query.genres;
+    const result = await pool.query(`SELECT * FROM movies WHERE genres = $1`, [
+      genresParam,
+    ]);
+    // แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
 
-		return res.json({
-			data: result.rows,
-		});
-	} catch (e) {
-		return res.json({
-			message: "ไม่สามารถเชื่อมต่อ Database ได้",
-		});
-	}
+    return res.json({
+      data: result.rows,
+    });
+  } catch (e) {
+    return res.json({
+      message: "ไม่สามารถเชื่อมต่อ Database ได้",
+    });
+  }
 });
 
 app.listen(port, () => {
-	console.log(`🚀 Server is running at ${port}`);
+  console.log(`🚀 Server is running at ${port}`);
 });
