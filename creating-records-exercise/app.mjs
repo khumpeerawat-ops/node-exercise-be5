62c4ad8ce4e4de69e36d63abd03f832a9895225b
panuwat <khumpeerawat@gmail.com>
2026-02-09 22:15:58 +0700

    API:creating new records

--- creating-records-exercise/app.mjs
+++ creating-records-exercise/app.mjs
@@ -7,7 +7,30 @@ const port = 4000;
 app.use(express.json());
 
 // 📍 **** สร้าง API เพื่อใช้ในการเพิ่มข้อมูลหนังเรื่องใหม่ไปที่ Database ตรงนี้ ****
+app.post("/movies", async (req, res) => {
+  const newMovie = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO movies (title, description, genres, year, poster, rating) VALUES ($1, $2, $3, $4, $5, $6)",
+      [
+        newMovie.title,
+        newMovie.description,
+        newMovie.genres,
+        newMovie.year,
+        newMovie.poster,
+        newMovie.rating,
+      ],
+    );
+    return res.status(201).json({
+      message: "Movie has been created.",
+    });
+  } catch (error) {
+    console.error("Error inserting movie:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
-	console.log(`🚀 Server is running at ${port}`);
+  console.log(`🚀 Server is running at ${port}`);
 });
